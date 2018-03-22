514e2925beaea744e1ce09141016e5561c4e1b01
Dave Woodall <dave.woodall@phishme.com>
2018-03-22 10:52:29 -0400

    animation station

---
+++ script.js
@@ -0,0 +1,12 @@
+$(function() {
+  $('.rect').click(function() {
+    $('.rect').toggleClass('animation');
+  });
+});
+
+// document.getElementById('close').addEventListener('click', function() {
+//   document.getElementById('close').innerHTML = 'Hello World';
+// });
+//
+// var l = document.getElementById('close');
+// l.click();
